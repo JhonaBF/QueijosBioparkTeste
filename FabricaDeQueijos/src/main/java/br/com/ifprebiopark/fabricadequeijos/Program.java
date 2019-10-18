e606f88bca58f3922227419ea7d52f28c550d6b3
Jhonata Bruno Fucks <jhonata.b.f@hotmail.com>
2019-10-17 22:25:52 -0300

    Adições

---
+++ FabricaDeQueijos/src/main/java/br/com/ifprebiopark/fabricadequeijos/Program.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifprebiopark.fabricadequeijos;
+
+import view.CadastroDeFornecedor;
+import view.EntregaDeLeite;
+
+/**
+ *
+ * @author jhona
+ */
+public class Program {
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(EntregaDeLeite.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(EntregaDeLeite.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(EntregaDeLeite.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(EntregaDeLeite.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EntregaDeLeite().setVisible(true);
+            }
+        });
+    }
+    
+}
